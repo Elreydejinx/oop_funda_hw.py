a4855d08c296e4b50a730a6c4197847b5e23bf75
Jeremiah-Williams <jae4lani17@gmail.com>
2024-07-01 20:12:13 -0400

    'oop_funda_hw.py'

---
+++ messingaround.py
@@ -0,0 +1,17 @@
+with open('my_thoughts.txt', 'w') as file:
+    file.write('\nSo I am determined to get this done i will not be left behind i just have to focus more and still handle my business at home...\nI wish my was on some Limitless type ish for real though...\nI really am trying to get this')
+with open('my_thoughts.txt', 'a') as file:
+    file.write('\nI think im getting this, im not sure though.')
+with open('my_thoughts.txt', 'a') as file:
+    file.write('\nNote: Will this work or not.')
+
+with open('my_thoughts.txt', 'a') as file:
+    file.write('\nWith ever great lesson i get a lil closer to understanding this I believe.')
+
+from datetime import date
+
+with open('my_thoughts.txt', 'a') as file:
+    file.write(f'\n{date.today()}: Watched Game of thrones')
+
+with open('my_thoughts.txt', 'a') as file:
+    file.write('\nSo hoping to get this down packed cuz i really want to learn this')
---
+++ oop_funda_hw.py
@@ -0,0 +1,35 @@
+# Task 1: Vehicle Registration System
+
+# Problem Statement: Create a Python class Vehicle with attributes registration_number, type, and owner. Implement a method update_owner to change the vehicle's owner.
+# Then, create a few instances of Vehicle and demonstrate changing the owner.
+# Code Example: Provide a basic structure for the Vehicle class without methods.
+class Vehicle:
+    def __init__(self, reg_num, type, owner):
+            self.registration_number = reg_num
+            self.type = type
+            self.owner = owner
+           
+    def update_user(self):
+         self.owner = input('enter the name of the new owner:')
+         print(self.owner)
+
+Honda_fit = Vehicle('1VH658567568', 'Sadan', 'James Kennedy')
+Jeep_charechee = Vehicle('1H1234124124', 'SUV', 'James Kennedy')
+Dogde_ram = Vehicle('1VHDDF231084', 'Pick up', 'James Kennedy')
+
+Honda_fit.update_user()
+     
+# Expected Outcome: Completion of the Vehicle class with the update_owner method and 
+# a demonstration script showing the creation of Vehicle objects and updating their owners.
+# Task 2: Event Management System Enhancement
+
+# Problem Statement: Use the existing Event class by adding an attribute to keep track of the number of participants. 
+# Implement a method add_participant that increases the count and a method get_participant_count to retrieve the current count.
+# Code Example: Basic Event class without participant tracking.
+class Event:
+    def __init__(self, name, date):
+        self.name = name
+        self.date = date
+            
+            
+    
